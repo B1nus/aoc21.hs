fa9327e82cf88e8abf684a400d40411776348530
Lini <119787571+B1nus@users.noreply.github.com>
2025-11-18 22:55:45 +0100

    Day 9 Part 2

--- 9.hs
+++ 9.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 type Map = [[Int]]
 
 parse :: String -> Map
@@ -9,19 +11,29 @@ w = length . head
 h :: Map -> Int
 h = length
 
-risk :: Map -> Int -> Int -> Int
-risk m x y | x >= 0 && x < w m && y >= 0 && y < h m = 1 + m!!y!!x
-           | otherwise                              = 11
+risk :: Map -> (Int, Int) -> Int
+risk m (x,y) = if ins m (x,y) then m!!y!!x else 11
 
-isLow :: Map -> Int -> Int -> Bool
-isLow m x y = c < l && c < r && c < t && c < b
-  where c = risk m x y
-        l = risk m (x - 1) y
-        r = risk m (x + 1) y
-        t = risk m x (y - 1)
-        b = risk m x (y + 1)
+ins :: Map -> (Int, Int) -> Bool
+ins m (x, y) = x `elem` [0..w m - 1] && y `elem` [0..h m - 1]
 
-lows :: Map -> [Int]
-lows m = [risk m x y | x <- xs, y <- ys, isLow m x y]
-  where xs = [0..(w m - 1)]
-        ys = [0..(h m - 1)]
+adj :: Map -> (Int, Int) -> [(Int, Int)]
+adj m (x, y) = filter (ins m) [(x - 1, y),(x,y - 1),(x + 1,y),(x,y + 1)]
+
+isLower m p = (risk m p <) . risk m
+isHigher m p = (risk m p >) . risk m
+
+lows :: Map -> [(Int, Int)]
+lows m = [(x, y) | x <- xs, y <- ys, isLow m (x, y)]
+  where xs          = [0..(w m - 1)]
+        ys          = [0..(h m - 1)]
+        isLow m p = all (isLower m p) $ adj m p
+
+basin :: Map -> (Int, Int) -> [(Int, Int)]
+basin m (x,y) = basin' [(x,y)]
+  where basin' ps | ps' == ps = ps
+                  | otherwise = basin' ps'
+          where ps'    = nub $ new ps ++ ps
+                new ps = concat [new' (x,y) | (x,y) <- ps]
+                new' p = filter ((/= 9) . risk m) $ filter (isLower m p) $ adj m p
+
